e99a5622c7ba18b84dc36a979e4833be8edbd599
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2022-03-02 17:37:48 +0600

    Alhamdulillah completed

--- js/app.js
+++ js/app.js
@@ -1,69 +1,33 @@
 const container = document.getElementById('search-results');
 const phoneDetail = document.getElementById("phone-details");
+
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
 const searchPhone = () => {
     const serchField = document.getElementById('search-field');
     const message = document.getElementById('message');
     const serchFieldValue = serchField.value;
     serchField.value = '';
+    toggleSpinner('block')
     const url = `https://openapi.programming-hero.com/api/phones?search=${serchFieldValue}`;
     fetch(url)
         .then(res => res.json())
         .then(data => {
             if (data.data[0] == null) {
                 message.style.display = 'block';
-                // serchField.value = '';
+                toggleSpinner('none');
                 container.innerHTML = '';
                 phoneDetail.textContent = '';
-
             }
 
-            // else if (data.data[0]) {
-            //     message.innerHTML = '';
-            //     phoneDetail.textContent = '';
-            //     //     // serchField.value = ''; 
-
-            //     //     //     message.style.display = 'none';
-            //     //     //     container.textContent = '';
-            // }
             else {
                 displayPhone(data.data[0])
                 message.style.display = 'none';
-                // container.innerHTML = '';
                 phoneDetail.textContent = '';
-                // serchField.value = '';
-
             }
         })
 }
-
-
-/* const loadPhone = searchText => {
-//     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-//     // console.log(url)
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(data => displayPhone(data.status))
-// }
-
- if (isNaN(inputValue) || inputValue == "") {
-        error.innerText = "please give a number";
-        input.value = '';
-        main.innerHTML = '';
-    }
-    else if (inputValue < 0) {
-        error.innerText = "please enter a positive number";
-        input.value = '';
-        main.innerHTML = '';
-    }
-    else {
-        fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${inputValue}`)
-            .then(res => res.json())
-            .then(data => cardDisplay(data.cards))
-        input.value = '';
-        error.innerHTML = '';
-    }
-
-  loadPhone('data');*/
 
 const displayPhone = data => {
     console.log(data)
@@ -71,7 +35,6 @@ const displayPhone = data => {
     container.textContent = '';
     const pair = Object.entries(data)
     for (const pair in data) {
-        // console.log(pair)
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -80,18 +43,17 @@ const displayPhone = data => {
                   <div class="card-body">
                      <h5 class="card-title">${data.phone_name}</h5>
                      <p class="card-text">${data.brand}</p>
-                     <button onclick="loadPhoneDetail('${data.slug}')" class="btn btn-primary ms-auto">See details</button>
+                     <button onclick="loadPhoneDetail('${data.slug}')" class="btn btn-primary mx-5">See details</button>
                   </div>
              </div>
                   `;
         container.appendChild(div);
     }
+    toggleSpinner('none');
 }
 
 const loadPhoneDetail = id => {
-    // console.log(id)
-    const url = (`https://openapi.programming-hero.com/api/phone/${id}`)
-    // console.log(url)
+    const url = (`https://openapi.programming-hero.com/api/phone/${id}`);
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhoneDetail(data.data))
@@ -101,30 +63,28 @@ const displayPhoneDetail = data => {
     console.log(data)
     const phoneDetail = document.getElementById("phone-details");
     phoneDetail.textContent = '';
-    const pair = Object.entries(data.mainFeatures);
-    for (let pair in data) {
-        const div = document.createElement('div');
-        div.classList.add('col')
-        div.innerHTML = ` <div class="card" style="width: 18rem;">
+    const div = document.createElement('div');
+    div.classList.add('col')
+    div.innerHTML = ` <div class="card" style="width: 18rem;">
                 <img src="${data.image}" class="card-img-top" alt="...">
                 <div class="card-body bg-dark text-white">
                    <h5 class="card-title">${data.name}</h5>
-                   <p class="card-text">releaseDate: ${data.releaseDate}</p>
-                   <p class="card-text">chipSet: ${data.mainFeatures.chipSet}</p>
-                   <p class="card-text">displaySize: ${data.mainFeatures.displaySize}</p>
-                   <p class="card-text">memory: ${data.mainFeatures.memory}</p>
-                   <p class="card-text">storage: ${data.mainFeatures.storage}</p>
-                   <p class="card-text">sensors: ${data.mainFeatures.sensors}</p>   
-                   <p class="card-text">Bluetooth: ${data.others.Bluetooth}</p>   
-                   <p class="card-text">GPS: ${data.others.GPS}</p>   
-                   <p class="card-text">NFC: ${data.others.NFC}</p>   
-                   <p class="card-text">Radio: ${data.others.Radio}</p>   
-                   <p class="card-text">USB: ${data.others.USB}</p>   
-                   <p class="card-text">WLAN: ${data.others.WLAN}</p>   
+                   <p class="card-text">releaseDate : ${data.releaseDate}</p>
+                   <p class="card-text">chipSet : ${data.mainFeatures.chipSet}</p>
+                   <p class="card-text">displaySize : ${data.mainFeatures.displaySize}</p>
+                   <p class="card-text">memory : ${data.mainFeatures.memory}</p>
+                   <p class="card-text">storage : ${data.mainFeatures.storage}</p>
+                   <p class="card-text">sensors : ${data.mainFeatures.sensors}</p> 
+                   <p class="card-text">Bluetooth : ${data.others?.Bluetooth}</p>   
+                   <p class="card-text">GPS : ${data.others?.GPS}</p>   
+                   <p class="card-text">NFC : ${data.others?.NFC}</p>   
+                   <p class="card-text">Radio : ${data.others?.Radio}</p>   
+                   <p class="card-text">USB : ${data.others?.USB}</p>   
+                   <p class="card-text">WLAN : ${data.others?.WLAN}</p>
                   </div>
                 </div>
                 `;
-        phoneDetail.appendChild(div);
-    }
+    phoneDetail.appendChild(div);
 
+    toggleSpinner('none');
 }
